d81d4ee2deda8447f94d0dddbccc59e69fd44515
togatoga <togasakitogatoga@gmail.com>
2018-03-15 20:29:59 +0900

    Update chrome/bookmark.py

--- chrome/bookmark.py
+++ chrome/bookmark.py
@@ -2,18 +2,17 @@ import platform
 
 class Bookmark:
     def __init__(self):
-        pass
-    
-    def __get_bookmark_path(self):
+        self.__set_bookmarks_path()
+    def __set_bookmarks_path(self):
         system_name = platform.system()
-        print (system_name)
         if system_name == "Linux":
-            self.bookmark_path = "~/.config/google-chrome/Default/Bookmarks"
+            self.bookmarks_path = "~/.config/google-chrome/Default/Bookmarks"
         elif system_name == "Darwin":
-            self.bookmark_path = '~/Library/Application Support/Google/Chrome/Default/Bookmarks'
+            self.bookmarks_path = '~/Library/Application Support/Google/Chrome/Default/Bookmarks'
         else:
             pass
-        
+    def __get_bookmarks(self):
+        pass
     
     
             
